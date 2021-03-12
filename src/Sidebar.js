b414eac2c4dfd5fd7d4db530464c5429c35f0c60
Aaron Yu <ayybuddyy@gmail.com>
2021-03-12 15:59:08 -0500

    Finish styling Sidebar and SidebarOption component and add in button for Sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -10,6 +10,7 @@ import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
 import ListAltIcon from '@material-ui/icons/ListAlt';
 import PermIdentityIcon from '@material-ui/icons/PermIdentity';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import { Button } from '@material-ui/core';
 
 const Sidebar = () => {
   return (
@@ -23,6 +24,10 @@ const Sidebar = () => {
       <SidebarOption Icon={ListAltIcon} text='Lists' />
       <SidebarOption Icon={PermIdentityIcon} text='Profile' />
       <SidebarOption Icon={MoreHorizIcon} text='More' />
+
+      <Button variant='outlined' className='sidebar__tweet' fullWidth>
+        Tweet
+      </Button>
     </div>
   );
 };
