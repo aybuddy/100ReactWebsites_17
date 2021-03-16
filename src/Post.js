b77bbf86d53099e01a011afa107cfd35103a5e18
Aaron Yu <ayybuddyy@gmail.com>
2021-03-16 10:21:08 -0400

    Install and implement react-flip-move for tweet texts

--- src/Post.js
+++ src/Post.js
@@ -1,3 +1,4 @@
+import React, { forwardRef } from 'react';
 import { Avatar } from '@material-ui/core';
 import {
   ChatBubbleOutline,
@@ -6,40 +7,41 @@ import {
   Repeat,
   VerifiedUser,
 } from '@material-ui/icons';
-import React from 'react';
 import './Post.css';
 
-const Post = ({ displayName, userName, verified, text, image, avatar }) => {
-  return (
-    <div className='post'>
-      <div className='post__avatar'>
-        <Avatar src={avatar} />
-      </div>
-      <div className='post__body'>
-        <div className='post__header'>
-          <div className='post__headerText'>
-            <h3>
-              {displayName}
-              <span className='post__headerSpecial'>
-                {verified && <VerifiedUser className='post__badge' />}@
-                {userName}
-              </span>
-            </h3>
+const Post = forwardRef(
+  ({ displayName, userName, verified, text, image, avatar }, ref) => {
+    return (
+      <div className='post' ref={ref}>
+        <div className='post__avatar'>
+          <Avatar src={avatar} />
+        </div>
+        <div className='post__body'>
+          <div className='post__header'>
+            <div className='post__headerText'>
+              <h3>
+                {displayName}
+                <span className='post__headerSpecial'>
+                  {verified && <VerifiedUser className='post__badge' />}@
+                  {userName}
+                </span>
+              </h3>
+            </div>
+            <div className='post__headerDescription'>
+              <p>{text}</p>
+            </div>
           </div>
-          <div className='post__headerDescription'>
-            <p>{text}</p>
+          <img src={image} alt='' />
+          <div className='post__footer'>
+            <ChatBubbleOutline fontSize='small' />
+            <Repeat fontSize='small' />
+            <FavoriteBorder fontSize='small' />
+            <Publish fontSize='small' />
           </div>
         </div>
-        <img src={image} alt='' />
-        <div className='post__footer'>
-          <ChatBubbleOutline fontSize='small' />
-          <Repeat fontSize='small' />
-          <FavoriteBorder fontSize='small' />
-          <Publish fontSize='small' />
-        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
 
 export default Post;
