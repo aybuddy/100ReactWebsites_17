d9ec0c87cf427a1102af193d57e54bfb07de1b19
Aaron Yu <ayybuddyy@gmail.com>
2021-03-16 09:31:35 -0400

    Install react-twitter-embed to grab tweets and display in Widgets component

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import './Widget.css';
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed,
+} from 'react-twitter-embed';
+import { Search } from '@material-ui/icons';
 
 const Widgets = () => {
   return (
     <div className='widgets'>
-      <h1>The Widget</h1>
+      <div className='widgets__input'>
+        <Search className='widgets__searchIcon' />
+        <input type='text' placeholder='Search Twitter' />
+      </div>
+
+      <div className='widgets__widgetContainer'>
+        <h2>What's happening</h2>
+        <TwitterTweetEmbed tweetId={'1370950463017861123'} />
+        <TwitterTimelineEmbed
+          sourceType='profile'
+          screenName='elonmusk'
+          options={{ height: 400 }}
+        />
+        <TwitterShareButton
+          url={''}
+          options={{ text: 'reactjs is awesome', via: 'elonmusk' }}
+        />
+      </div>
     </div>
   );
 };
