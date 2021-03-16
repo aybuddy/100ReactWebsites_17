3bfca66fb19186cdf04192a68f5737d5ab4e4ba9
Aaron Yu <ayybuddyy@gmail.com>
2021-03-16 10:00:58 -0400

    Connect firebase to app, pull data down from firebase and display in Feed component

--- src/Feed.js
+++ src/Feed.js
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Feed.css';
 import Post from './Post';
 import TweetBox from './TweetBox';
+import db from './firebase';
 
 const Feed = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection('posts').onSnapshot((snapshot) =>
+      setPosts(snapshot.docs.map((doc) => doc.data()))
+    );
+  }, []);
+
   return (
     <div className='feed'>
       <div className='feed__header'>
         <h2>Home</h2>
       </div>
       <TweetBox />
-      <Post
-        displayName='Elon Musk'
-        userName='elonmusk'
-        verified
-        text='Man on Mars'
-        avatar='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSvdl382q8a8hXT9ZSG6hASePoNH_bqAa4Lg&usqp=CAU'
-        image='https://media.giphy.com/media/vQS8z5J8qrCAE/giphy.gif'
-      />
+
+      {posts.map((post) => (
+        <Post
+          displayName={post.displayName}
+          userName={post.userName}
+          verified
+          text={post.text}
+          avatar={post.avatar}
+          image={post.image}
+        />
+      ))}
+
       <Post />
     </div>
   );
