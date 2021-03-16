6a8940cfeb24e522047ffce648fd5742fdedef18
Aaron Yu <ayybuddyy@gmail.com>
2021-03-15 20:58:20 -0400

    Create TweetBox component and connect to Feed component

---
+++ src/TweetBox.js
@@ -0,0 +1,19 @@
+import { Avatar, Button } from '@material-ui/core';
+import React from 'react';
+import './TweetBox.css';
+
+const TweetBox = () => {
+  return (
+    <div className='tweetBox'>
+      <form>
+        <div className='tweetBox__input'>
+          <Avatar src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSvdl382q8a8hXT9ZSG6hASePoNH_bqAa4Lg&usqp=CAU' />
+          <input type='text' placeholder='What is Happening?' />
+        </div>
+        <Button>Tweet</Button>
+      </form>
+    </div>
+  );
+};
+
+export default TweetBox;
